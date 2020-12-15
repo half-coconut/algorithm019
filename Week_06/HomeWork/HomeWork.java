f41a3d6e0b55db3e309207f6778181ec9035e9b3
half-coconut <584947559@qq.com>
2020-12-15 14:28:05 +0800

    homeWork 06

---
+++ Week_06/HomeWork/HomeWork.java
@@ -0,0 +1,224 @@
+package com.coconut.Day38_H.HomeWork;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author: coconut
+ * Description: TODO
+ * Date: 2020/12/15 14:13
+ * File: HomeWork
+ * Project: leetcode_cc
+ */
+
+
+public class HomeWork {
+    /**
+     * 860. 柠檬水找零
+     *
+     * @param bills
+     * @return
+     */
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            if (bill == 5)
+                five++;
+            else if (bill == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 1. 两数之和
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[0];
+    }
+
+    /**
+     * 198. 打家劫舍
+     *
+     * @param nums
+     * @return
+     */
+    public int rob(int[] nums) {
+        int len = nums.length;
+        if (len == 0)
+            return 0;
+        int[] dp = new int[len + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 2; i <= len; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+        return dp[len];
+    }
+
+    /**
+     * 53. 最大子序和
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int ans = nums[0];
+        int sum = 0;
+        for(int i: nums) {
+            if(sum > 0) {
+                sum += i;
+            } else {
+                sum = i;
+            }
+            ans = Math.max(ans, sum);
+        }
+        return ans;
+    }
+
+    /**
+     * 1143. 最长公共子序列
+     *
+     * @param text1
+     * @param text2
+     * @return
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        // 确保索引有效
+        int[][] dp = new int[m + 1][n + 1];
+        // i来遍历m，j来遍历n
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // 获取两个串字符
+                char c1 = text1.charAt(i), c2 = text2.charAt(j);
+                if (c1 == c2) {
+                    // 去找它们前面各退一格的值加1即可
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                } else {
+                    //要么是text1往前退一格，要么是text2往前退一格，两个的最大值
+                    dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    /**
+     * 45. 跳跃游戏 II
+     *
+     * @param nums
+     * @return
+     */
+    public int jump(int[] nums) {
+        int length = nums.length;
+        int end = 0;
+        int maxPosition = 0;
+        int steps = 0;
+        for (int i = 0; i < length - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]);
+            if (i == end) {
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
+    }
+
+    /**
+     * 64. 最小路径和
+     *
+     * @param grid
+     * @return
+     */
+    public int minPathSum(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == 0 && j == 0) continue;
+                else if (i == 0) grid[i][j] = grid[i][j - 1] + grid[i][j];
+                else if (j == 0) grid[i][j] = grid[i - 1][j] + grid[i][j];
+                else grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+            }
+        }
+        return grid[grid.length - 1][grid[0].length - 1];
+    }
+
+    /**
+     * 322. 零钱兑换
+     *
+     * @param coins
+     * @param amount
+     * @return
+     */
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        // 用max填充dp
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    /**
+     * 213. 打家劫舍 II
+     *
+     * @param nums
+     * @return
+     */
+    public int rob2(int[] nums) {
+        // o和1 情况
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        // Arrays.copyOfRange 复制nums数组
+        // 调用 myRob，选择最大值。
+        return Math.max(myRob(Arrays.copyOfRange(nums, 0, nums.length - 1)),
+                myRob(Arrays.copyOfRange(nums, 1, nums.length)));
+    }
+
+    private int myRob(int[] nums) {
+        int pre = 0, cur = 0, tmp;
+        // dp方程：dp[n+1] = max(dp[n],dp[n-1]+num)
+        // foreach num为值
+        for (int num : nums) {
+            // tmp 为缓存，pre保留前一个值，cur保存当前最大值，最后输出最大值。
+            tmp = cur;
+            cur = Math.max(pre + num, cur);
+            pre = tmp;
+        }
+        return cur;
+    }
+
+}
