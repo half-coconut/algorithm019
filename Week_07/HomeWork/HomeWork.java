009a9470b36140e4fa4ed0e03d5d552960e518fe
half-coconut <584947559@qq.com>
2020-12-23 02:19:11 +0800

    homework week07

---
+++ Week_07/HomeWork/HomeWork.java
@@ -0,0 +1,317 @@
+package com.coconut.Day51_H.HomeWork;
+
+import com.coconut.Day50.Demo.Trie;
+
+import java.util.*;
+
+/**
+ * Author: coconut
+ * Description: TODO
+ * Date: 2020/12/23 2:07
+ * File: HomeWork
+ * Project: leetcode_cc
+ */
+
+
+public class HomeWork {
+
+    /**
+     * 363. 矩形区域不超过 K 的最大数值和
+     *
+     * @param matrix
+     * @param k
+     * @return
+     */
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        int rows = matrix.length, cols = matrix[0].length, max = Integer.MIN_VALUE;
+        // O(cols ^ 2 * rows)
+        for (int l = 0; l < cols; l++) { // 枚举左边界
+            int[] rowSum = new int[rows]; // 左边界改变才算区域的重新开始
+            for (int r = l; r < cols; r++) { // 枚举右边界
+                for (int i = 0; i < rows; i++) { // 按每一行累计到 dp
+                    rowSum[i] += matrix[i][r];
+                }
+                max = Math.max(max, dpmax(rowSum, k));
+                if (max == k) return k; // 尽量提前
+            }
+        }
+        return max;
+    }
+
+    // 在数组 arr 中，求不超过 k 的最大值
+    private int dpmax(int[] arr, int k) {
+        int rollSum = arr[0], rollMax = rollSum;
+        // O(rows)
+        for (int i = 1; i < arr.length; i++) {
+            if (rollSum > 0) rollSum += arr[i];
+            else rollSum = arr[i];
+            if (rollSum > rollMax) rollMax = rollSum;
+        }
+        if (rollMax <= k) return rollMax;
+        // O(rows ^ 2)
+        int max = Integer.MIN_VALUE;
+        for (int l = 0; l < arr.length; l++) {
+            int sum = 0;
+            for (int r = l; r < arr.length; r++) {
+                sum += arr[r];
+                if (sum > max && sum <= k) max = sum;
+                if (max == k) return k; // 尽量提前
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 376. 摆动序列
+     *
+     * @param nums
+     * @return
+     */
+    public int wiggleMaxLength(int[] nums) {
+        int down = 1, up = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1])
+                up = down + 1;
+            else if (nums[i] < nums[i - 1])
+                down = up + 1;
+        }
+        return nums.length == 0 ? 0 : Math.max(down, up);
+    }
+
+    /**
+     * 62. 不同路径
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    // 方法一：使用二维数组，根据dp方程：dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; i++) dp[0][i] = 1;
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    // 方法二: 使用一维数组，空间复杂度降低
+    public int uniquePaths2(int m, int n) {
+        int[] cur = new int[n];
+        // 用 1填满cur
+        Arrays.fill(cur, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                cur[j] += cur[j - 1];
+            }
+        }
+        return cur[n - 1];
+    }
+
+    /**
+     * 63. 不同路径 II
+     *
+     * @param obstacleGrid
+     * @return
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid == null || obstacleGrid.length == 0) {
+            return 0;
+        }
+
+        // 定义 dp 数组并初始化第 1 行和第 1 列。
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n && obstacleGrid[0][j] == 0; j++) {
+            dp[0][j] = 1;
+        }
+
+        // 根据状态转移方程 dp[i][j] = dp[i - 1][j] + dp[i][j - 1] 进行递推。
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    /**
+     * 127. 单词接龙
+     *
+     * @param beginWord
+     * @param endWord
+     * @param wordList
+     * @return
+     */
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        // 第 1 步：先将 wordList 放到哈希表里，便于判断某个单词是否在 wordList 里
+        Set<String> wordSet = new HashSet<>(wordList);
+        // 提前终止条件
+        if (wordSet.size() == 0 || !wordSet.contains(endWord)) {
+            return 0;
+        }
+        wordSet.remove(beginWord);
+
+        // 第 2 步：图的广度优先遍历，必须使用队列和表示是否访问过的 visited 哈希表
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        Set<String> visited = new HashSet<>();
+        visited.add(beginWord);
+
+        // 第 3 步：开始广度优先遍历，包含起点，因此初始化的时候步数为 1
+        int step = 1;
+        while (!queue.isEmpty()) {
+            int currentSize = queue.size();
+            for (int i = 0; i < currentSize; i++) {
+                // 依次遍历当前队列中的单词
+                String currentWord = queue.poll();
+                // 如果 currentWord 能够修改 1 个字符与 endWord 相同，则返回 step + 1
+                if (changeWordEveryOneLetter(currentWord, endWord, queue, visited, wordSet)) {
+                    return step + 1;
+                }
+            }
+            step++;
+        }
+        return 0;
+    }
+
+    /**
+     * 尝试对 currentWord 修改每一个字符，看看是不是能与 endWord 匹配
+     *
+     * @param currentWord
+     * @param endWord
+     * @param queue
+     * @param visited
+     * @param wordSet
+     * @return
+     */
+    private boolean changeWordEveryOneLetter(String currentWord, String endWord,
+                                             Queue<String> queue, Set<String> visited, Set<String> wordSet) {
+        char[] charArray = currentWord.toCharArray();
+        for (int i = 0; i < endWord.length(); i++) {
+            // 先保存，然后恢复
+            char originChar = charArray[i];
+            for (char k = 'a'; k <= 'z'; k++) {
+                if (k == originChar) {
+                    continue;
+                }
+                charArray[i] = k;
+                String nextWord = String.valueOf(charArray);
+                if (wordSet.contains(nextWord)) {
+                    if (nextWord.equals(endWord)) {
+                        return true;
+                    }
+                    if (!visited.contains(nextWord)) {
+                        queue.add(nextWord);
+                        // 注意：添加到队列以后，必须马上标记为已经访问
+                        visited.add(nextWord);
+                    }
+                }
+            }
+            // 恢复
+            charArray[i] = originChar;
+        }
+        return false;
+    }
+
+    /**
+     * 208. 实现 Trie (前缀树)
+     */
+    class Trie {
+        private TrieNode root;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        /**
+         * Inserts a word into the trie.
+         */
+        public void insert(String word) {
+            TrieNode node = root;
+            for (int i = 0; i < word.length(); i++) {
+                char currentChar = word.charAt(i);
+                if (!node.containsKey(currentChar)) {
+                    node.put(currentChar, new TrieNode());
+                }
+                node = node.get(currentChar);
+            }
+            node.setEnd();
+        }
+
+        /**
+         * Returns if the word is in the trie.
+         */
+        public boolean search(String word) {
+            TrieNode node = searchPrefix(word);
+            return node != null && node.isEnd();
+        }
+
+
+        /**
+         * Returns if there is any word in the trie that starts with the given prefix.
+         */
+        public boolean startsWith(String prefix) {
+            TrieNode node = searchPrefix(prefix);
+            return node != null;
+
+        }
+
+        private TrieNode searchPrefix(String word) {
+            TrieNode node = root;
+            for (int i = 0; i < word.length(); i++) {
+                char currentLetter = word.charAt(i);
+                if (node.containsKey(currentLetter)) {
+                    node = node.get(currentLetter);
+                } else {
+                    return null;
+                }
+            }
+            return node;
+        }
+
+        class TrieNode {
+            private TrieNode[] links;
+
+            private final int R = 26;
+
+            private boolean isEnd;
+
+            public TrieNode() {
+                links = new TrieNode[R];
+            }
+
+            public boolean containsKey(char ch) {
+                return links[ch - 'a'] != null;
+            }
+
+            public TrieNode get(char ch) {
+                return links[ch - 'a'];
+            }
+
+            public void put(char ch, TrieNode node) {
+                links[ch - 'a'] = node;
+            }
+
+            public void setEnd() {
+                isEnd = true;
+            }
+
+            public boolean isEnd() {
+                return isEnd;
+            }
+        }
+    }
+
+}
